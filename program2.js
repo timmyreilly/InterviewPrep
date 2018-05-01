a75ab2ba79d973ff7f9e20dbe7a83a3f91a0aa4d
timmyreilly <timreilly@live.com>
2018-05-01 16:33:00 -0700

    getting learned

--- program2.js
+++ program2.js
@@ -88,27 +88,27 @@ If you want to use an arbitrary object as value of this, how will you do that?
 */
 
 var monica = {
-    name: 'monica', 
-    total: 500, 
-    deductMonthlyFee: function(fee) {
-        this.total = this.total - fee; 
-        return this.name + ' remaining balance is: ' + this.total; 
+    name: 'monica',
+    total: 500,
+    deductMonthlyFee: function (fee) {
+        this.total = this.total - fee;
+        return this.name + ' remaining balance is: ' + this.total;
     }
 }
 
-var rachel = {name: "Rachel", total: 1500}; 
-var rachelFeeDeductor = monica.deductMonthlyFee.bind(rachel, 200); 
+var rachel = { name: "Rachel", total: 1500 };
+var rachelFeeDeductor = monica.deductMonthlyFee.bind(rachel, 200);
 
-document.writeln(rachelFeeDeductor()); 
-document.writeln(rachelFeeDeductor()); 
+document.writeln(rachelFeeDeductor());
+document.writeln(rachelFeeDeductor());
 
 /*
 Cool, so you can bind a method to another object. 
 */
 
-var rachFeeDeductor = monica.deductMonthlyFee.bind(rachel); 
+var rachFeeDeductor = monica.deductMonthlyFee.bind(rachel);
 
-document.writeln(rachFeeDeductor(211)); 
+document.writeln(rachFeeDeductor(211));
 
 // That's neat too ^ 
 
@@ -116,3 +116,44 @@ document.writeln(rachFeeDeductor(211));
 8 Arguments and call: 
 
 */
+
+function isTwoPassed() {
+    var args = Array.prototype.slice.call(arguments);
+    return args.indexOf(2) != -1;
+}
+
+document.writeln(isTwoPassed(1, 5))
+document.writeln(isTwoPassed(4, 3, 2));
+
+// convert arguments to an array by calling slice method on an array and pass arguments. 
+
+// What the heck is this apply thing...
+
+function getMax(arr) {
+    return Math.max.apply(null, arr);
+}
+
+document.writeln(getMax([1, 2, 3, 4]));
+
+/* 10. this
+What the heck is this in JavaScript? 
+
+this always refers to an object. 
+this refers to the current execution context. 
+
+while executing a function in the context of an object, the object becomes the value of this. 
+dom event handler, value of this would be the element that fired the event. 
+*/
+
+document.write('typeof: ', typeof []); // object 
+
+document.write(' 2+true: ', 2 + true); // 3 
+document.write(" '6' + 9: ", '6' + 9); 
+document.write(" 4+3+2+'1': ", 4+3+2+'1' ) // '91' 
+
+document.write(" -'34'+10: ", -'34'+10); // -24 for crying out loud...
+
+document.write(" -'dude': ", -'dude'); 
+
+
+

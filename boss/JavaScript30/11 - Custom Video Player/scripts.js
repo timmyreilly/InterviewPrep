35fcbbce2f4c1f163dabc7be35b5118f471f8f1e
timmyreilly <timreilly@live.com>
2018-05-30 19:44:46 -0700

    no scrubs... actually yess scrubs

--- boss/JavaScript30/11 - Custom Video Player/scripts.js
+++ boss/JavaScript30/11 - Custom Video Player/scripts.js
@@ -29,10 +29,21 @@ function skip(e){
 }
 
 let x; 
+
 function handleRangeUpdate() {
-    console.log(this); 
-    x = this; 
     video[this.name] = this.value; 
+}
+
+function handleProgress(e) {
+    // we need a percentage. 
+    const percent = (video.currentTime / video.duration) * 100; 
+    progressBar.style.flexBasis = `${percent}%`; 
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth ) * video.duration; 
+    
+    video.currentTime = scrubTime; 
 }
 
 
@@ -41,6 +52,7 @@ function handleRangeUpdate() {
 video.addEventListener('click', togglePlay); 
 video.addEventListener('play', updateButton); 
 video.addEventListener('pause', updateButton); 
+video.addEventListener('timeupdate', handleProgress); 
 
 toggle.addEventListener('click', togglePlay); 
 
@@ -48,3 +60,11 @@ skipButtons.forEach( button => button.addEventListener('click', skip))
 
 ranges.forEach( range => range.addEventListener('change', handleRangeUpdate)); 
 ranges.forEach( range => range.addEventListener('mousemove', handleRangeUpdate)); 
+
+let mousedown = false; 
+progress.addEventListener('click', scrub); 
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+
+
+progress.addEventListener('mousedown', () => mousedown = true); 
+progress.addEventListener('mouseup', () => mousedown = false); 

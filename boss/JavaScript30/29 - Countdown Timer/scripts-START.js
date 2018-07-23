37de4eec7f5cc9b15f8a3a07afba5990da9be203
timmyreilly <timreilly@live.com>
2018-07-23 15:22:34 -0700

    hey a timer

---
+++ boss/JavaScript30/29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,25 @@
+let countdown; 
+const timerDisplay = document.querySelector('.display__time-left'); 
+
+function timer(seconds) {
+    const now = Date.now(); 
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);  
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000); 
+        // check if we should stop it! 
+        if (secondsLeft < 0) {
+            clearInterval(countdown); 
+            return; 
+        }
+        displayTimeLeft(secondsLeft); 
+    }, 1000); 
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60); 
+    const remainderSeconds = seconds % 60; 
+
+    console.log(minutes, remainderSeconds); 
+}

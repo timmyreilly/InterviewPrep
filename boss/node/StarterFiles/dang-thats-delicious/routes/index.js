e7704cd3928b6f51cc6ccb061345553aab70c2fd
timmyreilly <timreilly@live.com>
2019-01-02 14:05:19 -0800

    getting into auth

--- boss/node/StarterFiles/dang-thats-delicious/routes/index.js
+++ boss/node/StarterFiles/dang-thats-delicious/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController')
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -26,6 +27,12 @@ router.get('/stores/:slug', catchErrors(storeController.getStore));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag)); 
+
+// 1. validate the user
+// 2. 
+// 3. 
+
+
 
 
 module.exports = router;

9994e7ea5583874dd4c16d228259a3a6088d86f1
timmyreilly <timreilly@live.com>
2019-03-13 14:28:03 -0700

    some more web app functionality

--- boss/node/StarterFiles/dang-thats-delicious/controllers/storeController.js
+++ boss/node/StarterFiles/dang-thats-delicious/controllers/storeController.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 const multer = require('multer');
-const jimp = require('jimp'); 
-const uuid = require('uuid'); 
+const jimp = require('jimp');
+const uuid = require('uuid');
 
 const multerOptions = {
     storage: multer.memoryStorage(),
-    fileFilter: function(req, file, next) {
+    fileFilter: function (req, file, next) {
         const isPhoto = file.mimetype.startsWith('image/');
-        if(isPhoto){
+        if (isPhoto) {
             next(null, true);
         } else {
-            next({ message: 'That filetype isn\'t allowed!'}, false);
+            next({ message: 'That filetype isn\'t allowed!' }, false);
         }
     }
 };
@@ -30,24 +30,24 @@ exports.addStore = (req, res) => {
 
 exports.resize = async (req, res, next) => {
     // check if there is no new file to resize
-    if( !req.file) {
-        next(); 
-        return; 
+    if (!req.file) {
+        next();
+        return;
     }
-    const extension = req.file.mimetype.split('/')[1]; 
-    req.body.photo = `${uuid.v4()}.${extension}`; 
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
     const photo = await jimp.read(req.file.buffer);
     await photo.resize(800, jimp.AUTO);
-    
+
     await photo.write(`./public/uploads/${req.body.photo}`);
     // once we have written the photo to our filesystem, keep going! 
-    next(); 
+    next();
 }
 
 exports.upload = multer(multerOptions).single('photo');
 
 exports.createStore = async (req, res) => {
-    req.body.author = req.user._id; 
+    req.body.author = req.user._id;
     const store = await (new Store(req.body)).save();
     req.flash('success', `Succesfully created ${store.name}. Care to leave a review`);
     res.redirect(`/stores/${store.slug}`);
@@ -60,8 +60,8 @@ exports.getStores = async (req, res) => {
 }
 
 const confirmOwner = (store, user) => {
-    if(!store.author.equals(user._id)) {
-        throw Error('You must own a store in order to edit it'); 
+    if (!store.author.equals(user._id)) {
+        throw Error('You must own a store in order to edit it');
     }
 }
 
@@ -70,7 +70,7 @@ exports.editStore = async (req, res) => {
     const store = await Store.findOne({ _id: req.params.id });
 
     // 2. confirm they are the owner of the store
-    confirmOwner(store, req.user); 
+    confirmOwner(store, req.user);
 
     // 3. Render out the edit form so the use can 
     res.render('editStore', { title: `Edit ${store.name}`, store });
@@ -89,22 +89,39 @@ exports.updateStore = async (req, res) => {
 }
 
 exports.getStore = async (req, res, next) => {
-    const store = await Store.findOne({slug: req.params.slug}).populate('author');
-    if(!store) {
-        next(); 
-        return; 
+    const store = await Store.findOne({ slug: req.params.slug }).populate('author');
+    if (!store) {
+        next();
+        return;
     }
 
     // res.send(store); 
-    res.render("store", {store, title: store.name})
+    res.render("store", { store, title: store.name })
 }
 
 exports.getStoresByTag = async (req, res) => {
     const tag = req.params.tag
-    const tagQuery = tag || {$exists: true};
-    const tagsPromise = Store.getTagsList(); 
-    const storesPromise = Store.find({ tags: tagQuery});
+    const tagQuery = tag || { $exists: true };
+    const tagsPromise = Store.getTagsList();
+    const storesPromise = Store.find({ tags: tagQuery });
     const [tags, stores] = await Promise.all([tagsPromise, storesPromise]); // array destructuring
-    res.render('tag', { tags, title: 'Tags', tag, stores }); 
+    res.render('tag', { tags, title: 'Tags', tag, stores });
     //res.json(stores);  
 };
+
+exports.searchStores = async (req, res) => {
+    const stores = await Store
+    .find({
+        $text: {
+            $search: req.query.q
+        }
+    }, {
+        score: {$meta: 'textScore'}
+    })
+    .sort({
+        score: {$meta: 'textScore'}
+    })
+    .limit(5); 
+    
+    res.json(stores);
+}
---
+++ boss/node/StarterFiles/dang-thats-delicious/public/javascripts/modules/typeAhead.js
@@ -0,0 +1,74 @@
+const axios = require('axios'); 
+const dompurify = require('dompurify');  
+
+function searchResultsHTML(stores) {
+    return stores.map(store => {
+        return `
+        <a href="/store/${store.slug}" class="search__result">
+            <strong>${store.name}</strong>
+        </a>
+        `
+    }).join('')
+}
+
+function typeAhead(search) {
+    if(!search) return; 
+
+    const searchInput = search.querySelector('input[name="search"]');
+    const searchResults = search.querySelector('.search__results');
+    
+    searchInput.on('input', function() {
+        if(!this.value) {
+            // if there is no value, quit it! 
+            searchResults.style.display = 'none';
+            return; 
+        }
+
+        // show the search results!
+        searchResults.style.display = "block"; 
+
+        axios 
+            .get(`/api/search?q=${this.value}`)
+            .then(res => {
+                if(res.data.length){
+                    console.log('There is something to show');
+                    searchResults.innerHTML = dompurify.sanitize(searchResultsHTML(res.data));   
+                }
+            })
+            .catch(error => {
+                console.error(err); 
+            })
+    })
+
+    // handle keyboard inputs 
+    searchInput.on('keyup', (e) => {
+        // if they aren't pressing up, down or enter, who cares! 
+        if(![38, 40, 13].includes(e.keyCode)) {
+            return; 
+        }
+        
+        const activeClass = 'search__result--active'; 
+        const current = search.querySelector(`.${activeClass}`);
+        const items = search.querySelectorAll('.search__result'); 
+        let next; 
+        if(e.keyCode === 40 && current) {
+            next = current.nextElementSibling || items[0]; 
+        } else if (e.keyCode === 40) {
+            next = items[0]; 
+        } else if (e.keyCode === 38 && current) {
+            next = current.previousElementSibling || items[items.length - 1]
+        } else if (e.keyCode ==38) {
+            next = items[items.length - 1]; 
+        } else if (e.keyCode === 13 && current.href) {
+            window.location = current.href; 
+            return; 
+        }
+
+        if(current) {
+            current.classList.remove(activeClass); 
+        }
+        next.classList.add(activeClass); 
+    })
+}
+
+export default typeAhead; 
